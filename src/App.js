bde18ed350ff9f9f606856c37e285ed50f373bf8
inaki <inaki@inakis-MacBook-Pro.local>
2017-04-06 23:12:09 -0700

    First commit, avatar in place

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Avatar from './components/Avatar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Avatar />
+      </div>
+    );
+  }
+}
+
+export default App;
